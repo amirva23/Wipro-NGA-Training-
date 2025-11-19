dd07fcc41e3cc4658e7c1058e812440c622e4b54
amirva23 <tharshanamirva@gmail.com>
2025-11-19 18:03:28 +0530

    Day 20

---
+++ Day20_SkillTrack_API/middlewares/logger.js
@@ -0,0 +1,16 @@
+// Logging middleware with mimal functionality
+
+
+// Module to log HTTP method and URL of each request and then print to console ie method and URL
+
+module.exports = (req, res, next) => {
+
+  console.log(`${req.method} ${req.url}`);
+
+  next();
+
+};
+
+
+
+
